a1a385931d6dc8dad61adeda91f6ca26ae224b8f
Suwashshrestha <suwashshrestha5536@gmail.com>
2025-02-28 02:20:53 +0545

    changes

--- app/routes/user.watchlist.tsx
+++ app/routes/user.watchlist.tsx
@@ -1,6 +1,17 @@
 const watchlist = () => {
   return (
-    <div>watchlist</div>
+    <div className="flex min-h-screen items-center justify-center bg-gray-50">
+    <div className="text-center">
+      <h1 className="mb-4 text-4xl font-bold text-gray-800">
+        🚧 Under Construction 🚧
+      </h1>
+      <p className="text-lg text-gray-600">
+        We're working hard to build something amazing for you.
+        <br />
+        Please check back soon!
+      </p>
+    </div>
+  </div>
   )
 }
 
